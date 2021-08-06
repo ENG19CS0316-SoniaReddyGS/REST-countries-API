8bd52b3af4026eae3e4cff69598813f7617f40e9
ENG19CS0316-SoniaReddyGS <76095387+ENG19CS0316-SoniaReddyGS@users.noreply.github.com>
2021-08-06 13:47:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import renderHtml from './renderHtml.js';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+	renderHtml(req, res);	
+});
+
+app.get('/detail/:countryName', (req, res) => {
+	renderHtml(req, res);
+});
+
+app.use(express.static(path.resolve('./build')));
+
+app.listen(PORT, () => {
+  console.log(`App launched on ${PORT}`);
+});
